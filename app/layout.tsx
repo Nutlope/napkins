f6c89c76b07b234c7ec690195df278db355f18fc
Hassan El Mghari <hassan4709@gmail.com>
2024-09-25 16:39:27 -0400

    updated links

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
           <p>
             Powered by{" "}
             <a
-              href="https://dub.sh/together-ai"
+              href="https://dub.sh/llama3.2vision/?utm_source=example-app&utm_medium=napkins&utm_campaign=napkins-app-signup"
               target="_blank"
               className="font-bold hover:underline underline-offset-4"
             >
@@ -92,8 +92,7 @@ export default function RootLayout({
             </a>{" "}
             and{" "}
             <a
-              // TODO: Update this with blog post link
-              href="https://dub.sh/together-ai"
+              href="https://dub.sh/llama3.2vision/?utm_source=example-app&utm_medium=napkins&utm_campaign=napkins-app-signup"
               target="_blank"
               className="font-bold hover:underline underline-offset-4"
             >
